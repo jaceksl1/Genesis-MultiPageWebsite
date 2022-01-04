537429c6f2e96cb138ed273e93c1fd0d95c94f61
Jacek Słomian <j.slomian@3mk.local>
2022-01-04 14:22:40 +0100

    about section

--- js/script.js
+++ js/script.js
@@ -1,52 +1,58 @@
-let navbar = document.querySelector('.header .navbar')
+let navbar = document.querySelector(".header .navbar");
 
-document.querySelector('#menu-btn').onclick = () =>{
-  navbar.classList.add('active');
-}
-
-document.querySelector('#close-navbar').onclick = () =>{
-  navbar.classList.remove('active');
+document.querySelector("#menu-btn").onclick = () => {
+  navbar.classList.add("active");
 };
 
-let registerBtn = document.querySelector('.account-form .register-btn');
-let loginBtn = document.querySelector('.account-form .login-btn');
-
-registerBtn.onclick = () =>{
-  registerBtn.classList.add('active');
-  loginBtn.classList.remove('active');
-  document.querySelector('.account-form .login-form').classList.remove('active');
-  document.querySelector('.account-form .register-form').classList.add('active');
+document.querySelector("#close-navbar").onclick = () => {
+  navbar.classList.remove("active");
 };
 
-loginBtn.onclick = () =>{
-  registerBtn.classList.remove('active');
-  loginBtn.classList.add('active');
-  document.querySelector('.account-form .login-form').classList.add('active');
-  document.querySelector('.account-form .register-form').classList.remove('active');
+let registerBtn = document.querySelector(".account-form .register-btn");
+let loginBtn = document.querySelector(".account-form .login-btn");
+
+registerBtn.onclick = () => {
+  registerBtn.classList.add("active");
+  loginBtn.classList.remove("active");
+  document
+    .querySelector(".account-form .login-form")
+    .classList.remove("active");
+  document
+    .querySelector(".account-form .register-form")
+    .classList.add("active");
 };
 
-let accountForm = document.querySelector('.account-form')
+loginBtn.onclick = () => {
+  registerBtn.classList.remove("active");
+  loginBtn.classList.add("active");
+  document.querySelector(".account-form .login-form").classList.add("active");
+  document
+    .querySelector(".account-form .register-form")
+    .classList.remove("active");
+};
 
-document.querySelector('#account-btn').onclick = () =>{
-  accountForm.classList.add('active');
-}
+let accountForm = document.querySelector(".account-form");
 
-document.querySelector('#close-form').onclick = () =>{
-  accountForm.classList.remove('active');
+document.querySelector("#account-btn").onclick = () => {
+  accountForm.classList.add("active");
+};
+
+document.querySelector("#close-form").onclick = () => {
+  accountForm.classList.remove("active");
 };
 
 var swiper = new Swiper(".home-slider", {
   pagination: {
     el: ".swiper-pagination",
-    clickable:true,
+    clickable: true,
   },
-  loop:true,
-  grabCursor:true,
+  loop: true,
+  grabCursor: true,
 });
 
-var swiper = new Swiper(".home-courses-slider", {
-  loop:true,
-  grabCursor:true,
+var swiper = new Swiper(".home-family-slider", {
+  loop: true,
+  grabCursor: true,
   spaceBetween: 20,
   breakpoints: {
     0: {
@@ -61,9 +67,9 @@ var swiper = new Swiper(".home-courses-slider", {
   },
 });
 
-var swiper = new Swiper(".teachers-slider", {
-  loop:true,
-  grabCursor:true,
+var swiper = new Swiper(".experts-slider", {
+  loop: true,
+  grabCursor: true,
   spaceBetween: 20,
   breakpoints: {
     0: {
@@ -79,8 +85,8 @@ var swiper = new Swiper(".teachers-slider", {
 });
 
 var swiper = new Swiper(".reviews-slider", {
-  loop:true,
-  grabCursor:true,
+  loop: true,
+  grabCursor: true,
   spaceBetween: 20,
   breakpoints: {
     0: {
@@ -96,8 +102,8 @@ var swiper = new Swiper(".reviews-slider", {
 });
 
 var swiper = new Swiper(".logo-slider", {
-  loop:true,
-  grabCursor:true,
+  loop: true,
+  grabCursor: true,
   spaceBetween: 20,
   breakpoints: {
     0: {
@@ -118,18 +124,20 @@ var swiper = new Swiper(".logo-slider", {
   },
 });
 
-let accordion = document.querySelectorAll('.faq .accordion-container .accordion');
+let accordion = document.querySelectorAll(
+  ".faq .accordion-container .accordion"
+);
 
-accordion.forEach(acco =>{
-  acco.onclick = () =>{
-    accordion.forEach(dion => dion.classList.remove('active'));
-    acco.classList.toggle('active');
+accordion.forEach((acco) => {
+  acco.onclick = () => {
+    accordion.forEach((dion) => dion.classList.remove("active"));
+    acco.classList.toggle("active");
   };
 });
 
-document.querySelector('.load-more .btn').onclick = () =>{
-  document.querySelectorAll('.courses .box-container .hide').forEach(show =>{
-    show.style.display = 'block';
+document.querySelector(".load-more .btn").onclick = () => {
+  document.querySelectorAll(".courses .box-container .hide").forEach((show) => {
+    show.style.display = "block";
   });
-  document.querySelector('.load-more .btn').style.display = 'none';
+  document.querySelector(".load-more .btn").style.display = "none";
 };
